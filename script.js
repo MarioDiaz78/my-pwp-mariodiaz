f1f75b71df877f65824fad3126f04c1ddf77132c
Mario Diaz <mdiaz20@cnm.edu>
2023-05-14 20:13:44 -0600

    modifed all 3 files - index.html, script.js, and style.css

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    multiplier: .5,
+});
 
+scroll.on('scroll', () => {
+    const progress = scroll.scroll.instance.scroll.y / scroll.scroll.limit.y;
+
+    const elementToScale = document.querySelector(' .my-element');
+    const scaleValue = 1 + (progress * 0.5);
+    elementToScale.style.transform = `scale(${scaleValue})`;
+});
 
