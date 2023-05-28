1ea262675b944f35835e93fe3b375cf0aeba9fc0
Mario Diaz <mdiaz20@cnm.edu>
2023-05-27 22:11:09 -0600

    modified 3 files - index.html, script.js, style.css

--- script.js
+++ script.js
@@ -9,8 +9,52 @@ $(document).ready(function () {
 
         $('html, body').animate({
             scrollTop: scrollTo
-        }, 1500, function () {
+        }, 1000, function () {
             window.location.hash = hash;
         });
     });
 });
+
+
+const canvas = document.getElementById('matrix');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+
+const alphabet = katakana + latin + nums;
+
+const fontSize = 12;
+const columns = canvas.width/fontSize;
+
+const rainDrops = [];
+
+for( let x = 0; x < columns; x++ ) {
+    rainDrops[x] = Math.floor(Math.random()*canvas.height/fontSize);// modified this part to start at random points
+    // in site.
+}
+
+const draw = () => {
+    context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = '#0F0';
+    context.font = fontSize + 'px monospace';
+
+    for(let i = 0; i < rainDrops.length; i++)
+    {
+        const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+
+        if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+            rainDrops[i] = 0;
+        }
+        rainDrops[i]++;
+    }
+};
+setInterval(draw, 30);
+//setInterval(draw, 60);
