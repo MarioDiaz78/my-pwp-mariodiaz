93da962b70fc3f3e20c3dee57963a0dea273112e
Mario Diaz <mdiaz20@cnm.edu>
2023-06-20 12:01:52 -0600

    modified 2 files

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
 import * as THREE from 'three';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { EdgesGeometry, LineBasicMaterial, LineSegments } from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// Create a cube with translucent material
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, opacity: 0.5, transparent: true } );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
+// Create edges for the cube
+const edges = new EdgesGeometry( geometry );
+const edgeMaterial = new LineBasicMaterial( { color: 0xffffff, linewidth: 2 } );
+const line = new LineSegments( edges, edgeMaterial );
+
+scene.add( cube );
+scene.add( line );
+
+// Create 3D text
+const loader = new FontLoader();
+loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+    const textGeometry = new THREE.TextGeometry( 'Mario Diaz', {
+        font: font,
+        size: 0.2,
+        height: 0.05,
+    });
+    const textMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
+    const text = new THREE.Mesh(textGeometry, textMaterial);
+    text.position.set(-0.5, 0.2, 0);
+    scene.add(text);
+});
 
 function animate() {
     requestAnimationFrame( animate );
@@ -22,4 +45,5 @@ function animate() {
 
     renderer.render( scene, camera );
 }
+
 animate();
