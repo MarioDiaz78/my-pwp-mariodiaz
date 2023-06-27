f623952a9d2f9b9e4680be2b0c16744a8409449a
Mario Diaz <mdiaz20@cnm.edu>
2023-06-27 09:27:21 -0600

    modified script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ var tl = gsap.timeline({repeat: -1}); // Repeat indefinitely
 // Apply animation to each carousel item
 $(".carousel__item").each(function(index, element){
     tl.to(element, { duration: 1, scale: 1, opacity: 1, y: 0 }) // Animate in
-        .to(element, { duration: 1, scale: 0.5, opacity: 0, y: "-150px" }, "+=1"); // Animate out
+        .to(element, { duration: 1, scale: 0.5, opacity: 0}, "+=1"); // Animate out
 });
 
 tl.play();
